129fd4cf290a047816a4d912e7142f95f7593122
kozistr <kozistr@gmail.com>
2019-03-01 02:26:46 +0900

    update: codes

--- mnist_test.py
+++ mnist_test.py
@@ -18,7 +18,7 @@ w_reg = tf.contrib.layers.l2_regularizer(5e-4)
 def train(sess,
           input_shape=(None, 784), n_classes=10,
           n_feat=32, n_blocks=2,
-          optimizer="adabound", lr=1e-3, grad_clip=1.,
+          optimizer="adabound", lr=1e-3, grad_clip=0.,
           log_dir="./logs"):
     def prepare_optimizer(optimizer_name="adabound"):
         if optimizer_name == "adabound":
@@ -48,13 +48,13 @@ def train(sess,
                                      kernel_initializer=w_init, kernel_regularizer=w_reg)
                 x = tf.nn.leaky_relu(x, alpha=0.2)
                 x = tf.nn.dropout(x, keep_prob=do_rate)
-                x = tf.layers.max_pooling2d(x, pool_size=(3, 3), strides=(2, 2), padding='SAME') \
+                x = tf.layers.max_pooling2d(x, pool_size=(2, 2), strides=(2, 2), padding='SAME') \
                     if n_layer_idx % 2 == 0 else x
                 n_feat *= 2
 
         x = tf.layers.flatten(x)
 
-        x = tf.layers.dense(x, units=512,
+        x = tf.layers.dense(x, units=256,
                             kernel_initializer=w_init, kernel_regularizer=w_reg)
         x = tf.nn.leaky_relu(x, alpha=0.2)
         x = tf.nn.dropout(x, keep_prob=do_rate)
@@ -68,20 +68,22 @@ def train(sess,
 
     with tf.name_scope("train"):
         global_step = tf.train.get_or_create_global_step()
-        optimizer = prepare_optimizer(optimizer)
+        opt = prepare_optimizer(optimizer)
 
         t_vars = tf.trainable_variables()
         grads = tf.gradients(loss, t_vars)
 
         # gradient clipping
-        grads, _ = tf.clip_by_global_norm(grads, clip_norm=grad_clip)
+        if grad_clip:
+            grads, _ = tf.clip_by_global_norm(grads, clip_norm=grad_clip)
 
-        train_op = optimizer.apply_gradients(zip(grads, t_vars), global_step=global_step)
+        train_op = opt.apply_gradients(zip(grads, t_vars), global_step=global_step)
 
         # Normally the global step update is done inside of `apply_gradients`.
         # However, my version of `AdaBoundyOptimizer` doesn't do this. But if you use
         # a different optimizer, you should probably take this line out.
-        train_op = tf.group(train_op, [global_step.assign(global_step + 1)])
+        if optimizer == "adabound":
+            train_op = tf.group(train_op, [global_step.assign(global_step + 1)])
 
     with tf.name_scope("metric"):
         corr_pred = tf.equal(tf.argmax(pred, axis=1), tf.argmax(label, axis=1))
@@ -194,13 +196,16 @@ if __name__ == "__main__":
     parser.add_argument('--optimizer', required=False, type=str, default="adabound")
     parser.add_argument('--filters', required=False, type=int, default=32)
     parser.add_argument('--n_blocks', required=False, type=int, default=4)
-    parser.add_argument('--dropout', required=False, type=float, default=0.25)
+    parser.add_argument('--dropout', required=False, type=float, default=0.5)
     parser.add_argument('--model_dir', required=False, type=str, default="./model/")
     parser.add_argument('--data_dir', required=False, type=str, default="./mnist/")
     parser.add_argument('--log_dir', required=False, type=str, default="./logs")
     parser.add_argument('--logging_steps', required=False, type=int, default=1000)
     parser.add_argument('--seed', required=False, type=int, default=1337)
     args = vars(parser.parse_args())
+
+    for k, v in args.items():
+        print("[+] {} : {}".format(k, v))
 
     # reproducibility
     tf.set_random_seed(args["seed"])
