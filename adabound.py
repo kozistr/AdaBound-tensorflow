6e5a0d9f63eaae479e972ba54993661ecc068411
Hyeongchan Kim <kozistr@gmail.com>
2019-03-01 14:57:23 +0900

    fix: variable name mismatch, #2

--- adabound.py
+++ adabound.py
@@ -132,9 +132,9 @@ class AdaBoundOptimizer(tf.train.Optimizer):
 
     def _do_use_weight_decay(self, param_name):
         """Whether to use L2 weight decay for `param_name`."""
-        if not self.weight_decay_rate:
+        if not self._weight_decay:
             return False
-        if self.exclude_from_weight_decay:
+        if self._exclude_from_weight_decay:
             for r in self.exclude_from_weight_decay:
                 if re.search(r, param_name) is not None:
                     return False
